cabd001ae82299875422f3e06b0fc936032d76d4
hungrytech <74886812+hungrytech@users.noreply.github.com>
2021-03-31 02:22:16 +0900

    Add JPAQueryFactory on beanFactory

--- src/main/java/ToyProject1/hungrytech/HungrytechApplication.java
+++ src/main/java/ToyProject1/hungrytech/HungrytechApplication.java
@@ -1,8 +1,12 @@
 package ToyProject1.hungrytech;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,5 +15,11 @@ public class HungrytechApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HungrytechApplication.class, args);
 	}
+
+	@Bean
+	public JPAQueryFactory queryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
+
 
 }
