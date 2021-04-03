14f81e20b921c9165f6db8453126a6399b1b34a1
hungrytech <74886812+hungrytech@users.noreply.github.com>
2021-04-03 23:07:19 +0900

    Add BoardComment entity

---
+++ src/main/java/ToyProject1/hungrytech/boardcommentDto/BoardCommentForm.java
@@ -0,0 +1,14 @@
+package ToyProject1.hungrytech.boardcommentDto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class BoardCommentForm {
+
+    private String content;
+
+
+}
