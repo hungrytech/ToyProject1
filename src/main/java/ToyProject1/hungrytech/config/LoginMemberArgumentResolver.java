f9f4c9f4c9d6e9003fd84f3b1d29288f8dd0ae6e
hungrytech <74886812+hungrytech@users.noreply.github.com>
2021-04-16 11:10:27 +0900

    Add LoginMember annotation

---
+++ src/main/java/ToyProject1/hungrytech/config/LoginMemberArgumentResolver.java
@@ -0,0 +1,39 @@
+package ToyProject1.hungrytech.config;
+
+import ToyProject1.hungrytech.memberDto.MemberLoginInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginMemberAnnotation = parameter.
+                getParameterAnnotation(LoginMember.class) != null;
+
+        boolean isMemberLoginInfoClass = MemberLoginInfo.class
+                .equals(parameter.getParameterType());
+
+        return isLoginMemberAnnotation && isMemberLoginInfoClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+       return httpSession.getAttribute("memberInfo");
+    }
+}
